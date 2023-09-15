2710bb1d8ac59acce4a412d70ea78d73e0cdb41b
Bruno de Jesus <bruno.jesus.carmo@gmail.com>
2023-09-15 15:44:14 -0300

    Initial Commit

---
+++ facebookLogin/facebookLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  facebookLoginApp.swift
+//  facebookLogin
+//
+//  Created by bruno on 15/09/23.
+//
+
+import SwiftUI
+
+@main
+struct facebookLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
